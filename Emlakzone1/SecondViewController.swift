14954cd72ded53f54caf08a5a2b22af92d833cc2
Ahsan <ahsanxz36@gmail.com>
2019-12-30 11:49:55 +0500

    Initial Commit

---
+++ Emlakzone1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Emlakzone1
+//
+//  Created by AL Rayyan Asif on 30/12/2019.
+//  Copyright © 2019 AL Rayyan Asif. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
